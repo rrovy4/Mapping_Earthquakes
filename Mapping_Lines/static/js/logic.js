d63a98404eafda0329aa75d6d105c4a812d1e3ed
oholrohan <53494166+oholrohan@users.noreply.github.com>
2021-06-01 12:24:38 -0400

    Adding files for Multiple_Lines...

--- Mapping_Lines/static/js/logic.js
+++ Mapping_Lines/static/js/logic.js
@@ -2,22 +2,44 @@
 console.log("working");
 
 // Create the map object with a center and zoom level.
-var myMap = L.map('mapid').setView([40.7, -94.5], 4);
+var myMap = L.map('mapid').setView([37.6213, -122.3790], 5);
+
+    // Define a symbol using SVG path notation, with an opacity of 1.
+    const lineSymbol = {
+        path: "M 0,-1 0,1",
+        strokeOpacity: 1,
+        scale: 4,
+      };
+
+// Coordinates for each point to be used in the line.
+let line = [
+    [33.9416, -118.4085],
+    [37.6213, -122.3790],
+    [40.7899, -111.9791],
+    [47.4502, -122.3088]
+  ];
+
+  // Create a polyline using the line coordinates and make the line red.
+L.polyline(line, {
+    color: "yellow",
+    dashArray: '10, 5',
+    //dashOffset: '15'
+}).addTo(myMap);
 
 // Get data from cities.js
 let cityData = cities;
 
 // Loop through the cities array and create one marker for each city.
-cityData.forEach(function(city) {
-    console.log(city)
-    L.circleMarker(city.location, {
-        radius: city.population/100000,
-        color: "orange",
-        fillColor: '#ffffa1'
-    })
-    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
-  .addTo(myMap);
-});
+// cityData.forEach(function(city) {
+//     console.log(city)
+//     L.circleMarker(city.location, {
+//         radius: city.population/100000,
+//         color: "orange",
+//         fillColor: '#ffffa1'
+//     })
+//     .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population.toLocaleString() + "</h3>")
+//   .addTo(myMap);
+// });
 
 //  Add a marker to the map for Los Angeles, California.
 // var marker = L.marker([34.0522, -118.2437]).addTo(myMap);
@@ -38,9 +60,10 @@ cityData.forEach(function(city) {
 var streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}', {
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
     maxZoom: 18,
-    id: 'dark-v10',
+    id: 'satellite-streets-v11',
     tileSize: 512,
     zoomOffset: -1,
+    //opacity: 0.5,
     accessToken: API_KEY
 });
 
